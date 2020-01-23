70770ae33595201b388a94969e855f7236ccf5b6
Felipe Alvares de Azevedo Macedo <felipeaamacedo1@gmail.com>
2020-01-23 11:15:24 -0300

    npm init -y used to create the package.json / npm i -s live-server used to install in the project the server that will automatically reload the browser / Written the start script that will start the server every time that npm start run / and learned the tsc -w that will watch for any change at the TypeScript file and recompile it at the browser

---
+++ introducao/basico.js
@@ -0,0 +1,3 @@
+"use strict";
+var a = 'To no TypeScript com direito a servidor ao vivo, bitchessss!!';
+console.log(a);
---
+++ introducao/basico.ts
@@ -0,0 +1,2 @@
+let a : string = 'To no TypeScript com direito a servidor ao vivo, bitchessss!!'
+console.log(a)
