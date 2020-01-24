d8da16d3d960bbf55e7b395268d0a78160c19394
Felipe Alvares de Azevedo Macedo <felipeaamacedo1@gmail.com>
2020-01-24 09:55:03 -0300

    Addition of array type of variable

--- tipos/tipo.js
+++ tipos/tipo.js
@@ -19,3 +19,9 @@ console.log(float);
 console.log("TIPO BOOLEAN");
 var vaiMudar = true;
 console.log(vaiMudar);
+// ARRAY
+// There are two to write ARRAYS (1) with the type of data inside the array plus brackets like shown bellow, (2) with the Element Array<typeofdata> where typeofdata may be string, number, or boolean.
+var hobbies = ['basketball', 'workout at the gym', 'photography', 'programming'];
+var list = [1, 2, 3, 4, 5];
+console.log(hobbies[0]);
+console.log(list[1]);
--- tipos/tipo.ts
+++ tipos/tipo.ts
@@ -26,4 +26,12 @@ console.log("TIPO BOOLEAN");
 let vaiMudar:boolean = true
 console.log(vaiMudar);
 
+// ARRAY
+// There are two to write ARRAYS (1) with the type of data inside the array plus brackets like shown bellow, (2) with the Element Array<typeofdata> where typeofdata may be string, number, or boolean.
+let hobbies : string[] = ['basketball', 'workout at the gym', 'photography', 'programming']
 
+let list : Array<number> = [1, 2, 3, 4, 5]
+
+
+console.log(hobbies[0]);
+console.log(list[1]);
