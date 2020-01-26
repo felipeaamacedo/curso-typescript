093fb835b6b00b16274b7eea520a023b1a460b8e
Felipe Alvares de Azevedo Macedo <felipeaamacedo1@gmail.com>
2020-01-26 12:54:08 -0300

    enum and any type added with an explanation of when it is used

--- tipos/tipo.ts
+++ tipos/tipo.ts
@@ -28,6 +28,9 @@ console.log(vaiMudar);
 
 // ARRAY
 // There are two to write ARRAYS (1) with the type of data inside the array plus brackets like shown bellow, (2) with the Element Array<typeofdata> where typeofdata may be string, number, or boolean.
+
+console.log("TIPO ARRAY");
+
 let hobbies : string[] = ['basketball', 'workout at the gym', 'photography', 'programming']
 
 let list : Array<number> = [1, 2, 3, 4, 5]
@@ -35,3 +38,42 @@ let list : Array<number> = [1, 2, 3, 4, 5]
 
 console.log(hobbies[0]);
 console.log(list[1]);
+
+// ENUM
+// Used when you need an specific list that doesn't fit in any of the types before. Exemples: Days of the Week, Actions in a movie...
+
+enum Cor{
+    Amarelo,
+    Vermelho = 100,
+    Verde = 2,
+    Azul,
+    Roxo,
+    Cinza
+} 
+
+let minhaCor : Cor = Cor.Cinza;
+
+console.log("Printing the value of minhaCor");
+console.log(minhaCor);
+
+console.log('Printing the color values of the list');
+console.log(Cor.Amarelo);
+console.log(Cor.Vermelho);
+console.log(Cor.Verde);
+console.log(Cor.Azul);
+console.log(Cor.Roxo);
+console.log(Cor.Cinza);
+
+
+//ANY
+//Is a way to make your variable to work in the same way as the JavaScript. It works this way because TS was made to be introduced stepwisely.
+
+let carro: any = 'BMW'
+console.log(carro)                  // This time it is a string
+carro = { marca:'BMW', ano:2019}
+console.log(carro);                 // This time it is an object
+
+// This is only achieved because of the tipe of ANY that works exactly like variables in javascript.
+
+
+
