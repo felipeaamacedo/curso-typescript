48415daf8d1aff7e5a06b81f5b9b9cd3aaaa3edb
Felipe Macedo <felipeaamacedo1@gmail.com>
2020-04-01 02:05:54 -0300

    Todo para configurar como instalar e usar o TypeScript

--- introducao/basico.js
+++ introducao/basico.js
@@ -1,4 +1,13 @@
-"use strict";
+// PRÉ REQUISITOS PARA RODAR O TYPESCRIPT
+// 1. Instalar o node.js
+// 2. Digitar cmd no windows e digitar o comando "npm i -g typescript" e em seguida checar a versão do typescript com o comando "tsc -v"
+// 3. Instalar o VS Code;
+// COMO COMPILAR UM ARQUIVO TYPESCRIPT MANUALMENTE
+// 1. pelo terminal digitar o comando "tsc <nome do arquivo>.ts" que irá gerar um arquivo "<nome do arquivo>.js"
+// 2. a partir daí é possível rodar o arquivo JavaScript pelo browser ou pelo próprio node, usando o comando "node <nome do arquivo>.js"
+// COMPILANDO ARQUIVOS TYPESCRIPT USANDO O CODE RUNNER
+// 1. instalar coderunner pela loja de extensões do VSCODE (comando Ctrl + Shift + X)
+// 2. instalar no command prompt o ts-node, através do comando "npm i -g ts-node" OBS: Um warn de dependencias foi gerado mas não tem problema
 var changeChapter = 'Chapter 1: básico';
 var space = '-----------------------------';
 console.log(changeChapter);
