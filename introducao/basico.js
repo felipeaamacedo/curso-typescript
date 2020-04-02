fab174440f85ff521acd4c2e79dd08ce00e17f4c
Felipe Macedo <felipeaamacedo1@gmail.com>
2020-04-02 00:40:44 -0300

    Termino da Seção 2 do Curso TYPESCRIPT

--- introducao/basico.js
+++ introducao/basico.js
@@ -1,17 +1,28 @@
+"use strict";
 // PRÉ REQUISITOS PARA RODAR O TYPESCRIPT
 // 1. Instalar o node.js
 // 2. Digitar cmd no windows e digitar o comando "npm i -g typescript" e em seguida checar a versão do typescript com o comando "tsc -v"
 // 3. Instalar o VS Code;
 // COMO COMPILAR UM ARQUIVO TYPESCRIPT MANUALMENTE
-// 1. pelo terminal digitar o comando "tsc <nome do arquivo>.ts" que irá gerar um arquivo "<nome do arquivo>.js"
-// 2. a partir daí é possível rodar o arquivo JavaScript pelo browser ou pelo próprio node, usando o comando "node <nome do arquivo>.js"
+// 1. digitar o comando "tsc -init" para iniciar o projeto de typescript e criar o arquivo "tsconfig.json"
+// 2. pelo terminal digitar o comando "tsc <nome do arquivo>.ts" que irá gerar um arquivo "<nome do arquivo>.js"
+// 3. a partir daí é possível rodar o arquivo JavaScript pelo browser ou pelo próprio node, usando o comando "node <nome do arquivo>.js"
 // COMPILANDO ARQUIVOS TYPESCRIPT USANDO O CODE RUNNER
 // 1. instalar coderunner pela loja de extensões do VSCODE (comando Ctrl + Shift + X)
 // 2. instalar no command prompt o ts-node, através do comando "npm i -g ts-node" OBS: Um warn de dependencias foi gerado mas não tem problema
+// EXECUTANDO DIRETO DO BROWSER
+// 1. criar um arquivo chamado "index.html" dentro da pasta root do projeto;
+// 2. adicionar nesse arquivo no campo de body um <script src='caminho para o arquivo'></script> nesse caso o caminho seria introcucao/basico.js
+// 3. na mesma pasta root, digitar npm init -y, para criar um arquivo de configuração chamado "package.json"
+// 4. em seguida instalar o live-server com o comando "npm i -s live-server"
+// 5. dentro do arquivo package.json na área "scripts" inserir o comando "start": "live-server"
+// OBS neste momento, já é possível usar o comando "npm start" dentro da pasta root do projeto para rodar o código.
+//6. Utilizar o comando npm start para iniciar o projeto no browser,
+//7. abrir um novo terminal e usar o comando "tsc -w" que o typescript fica em modo WATCH e qualquer modificação do arquivo, quando salvo, ele já é compilado e a modificação aparece no browser.
 var changeChapter = 'Chapter 1: básico';
 var space = '-----------------------------';
 console.log(changeChapter);
 console.log(space);
 // Início dos exercícios do capítulo.
-var a = 'To no TypeScript com direito a servidor ao vivo, bitchessss!!';
+var a = 'To no TypeScript com direito a servidor ao vivo, bitchessss!! 333333';
 console.log(a);
--- tipos/tipo.ts
+++ tipos/tipo.ts
@@ -8,6 +8,7 @@ console.log(space);
 
 //Início do capítulo 2 típos.
 
+// AULA 12 - TIPOS NUMÉRICOS E BOOLEANOS
 //string
 console.log("TIPO STRING");
 let nome:string = "João"
@@ -18,16 +19,20 @@ console.log("TIPO NUMBER - Pode ser integer ou float");
 let integer:number = 25
 let float:number = 10.5
 
-console.log(integer)
-console.log(float);
+console.log("Este é um inteiro " + integer)
+console.log("Este é um float, numero com ponto, quebrado " + float);
 
 //boolean
 console.log("TIPO BOOLEAN");
 let vaiMudar:boolean = true
 console.log(vaiMudar);
 
-// ARRAY
-// There are two to write ARRAYS (1) with the type of data inside the array plus brackets like shown bellow, (2) with the Element Array<typeofdata> where typeofdata may be string, number, or boolean.
+// AULA 14 - ARRAY E TIPOS
+// There are two to write ARRAYS 
+// (1) with the type of data inside the array plus brackets like shown bellow,
+// (2) with the Element Array<typeofdata> where typeofdata may be string, number, or boolean.
+// 
+// A diferença entre os dois é que o tipo array possui alguns metodos associados
 
 console.log("TIPO ARRAY");
 
@@ -39,8 +44,16 @@ let list : Array<number> = [1, 2, 3, 4, 5]
 console.log(hobbies[0]);
 console.log(list[1]);
 
+// AULA 15 - TUPLAS
+// A Tupla é uma array com diferentes tipos de dados
+let endereco: [string, number] = ["av. rodrigo fernando grilo", 587];
+console.log("O Meu endereço é: " + endereco);
+
+
+
+// AULA 16 - ENUMS
 // ENUM
-// Used when you need an specific list that doesn't fit in any of the types before. Exemples: Days of the Week, Actions in a movie...
+// Estrutura de lista que você usa valores pré-definidos, que não constam nos tipos apresentados anteriores. Exemples: Days of the Week, Actions in a movie...
 
 enum Cor{
     Amarelo,
@@ -64,7 +77,7 @@ console.log(Cor.Azul);
 console.log(Cor.Roxo);
 console.log(Cor.Cinza);
 
-
+// AULA 17 - O TIPO "ANY"
 //ANY
 //Is a way to make your variable to work in the same way as the JavaScript. It works this way because TS was made to be introduced stepwisely.
 
@@ -76,11 +89,8 @@ console.log(carro);                 // This time it is an object
 // This is only achieved because of the tipe of ANY that works exactly like variables in javascript.
 
 
-
-//Functions
+// AULA 19 - FUNÇÕES
 //Functions are very important when dealling with JS and TS because it is the main heart of the language.
-
-
 //A function that returns a string type of data.
 function retornaMeuNome():string{
     return nome;
@@ -91,8 +101,8 @@ console.log(retornaMeuNome());
 //Function that doesn't return anything, that is why it is a void type of function.
 function digaOi():void{
     console.log('Oi');
-    
 }
+
 digaOi()
 
 //Function that receives 2 number values, and returns another number value.
@@ -102,14 +112,31 @@ function multiplicar(numA:number, numB:number):number{
 
 console.log(multiplicar(2,5));
 
-//Funções como tipo
+// AULA 20 - FUNÇÕES COMO TIPO
 //No TS ou JS tem a possibilidade de atribuir uma função a uma variável.
+let dividePorDois = function (a:number, b:number):boolean {
+    let c = a*b;
+    let resto = c % 2;  // símbolo % quer dizer o REMAINDER ou seja o resto da divisão de C por 2.
+    if (resto == 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log( "O valor é divisível por 2? " + dividePorDois(2,2) );
+console.log( "O valor é divisível por 2? " + dividePorDois(3,5) );
+
+
+// Arrow funciona para falar que uma variável é do tipo função
+// let calculo <cria uma variável> : <:depois da variável que indica o tipo, como vem antes de um parênteses, o TypeScript entende que essa variável é do tipo função) (<input da função> x:number, y:number) => <valor que a função retorna> return.
+// Esse tipo função só é utilizado quando essa variável que é do tipo função recebe uma variável exatamente igual ao tipo que você atribuiu, como é o caso da função multiplicar.
 let calculo: (x:number, y:number) => number;
 //Ao definir uma variável como uma função, da forma como está acima, é importante pois quando atribuir uma função específica para ela, ela só vai aceitar funções iguais aos tipos atribuidos, ou seja, uma função como "multiplicar", ao tentar atribuir a função "digaOi" por exemplo, o TS pega um erro.
 calculo = multiplicar
 console.log(calculo(5,6));
 
-
+//AULA 21 - TIPO OBJETOS
 //OBJECTS
 //The correct way to infere the type of data in an object is saying the type of data before the values, as seen bellow.
 let usuario: { nome: string, idade: number } = {
@@ -121,7 +148,7 @@ usuario.nome = 'Jefferson';
 
 console.log(usuario.nome)
 
-
+//AULA 22 - DESAFIO TIPO OBJETOS
 //Desafio
 /* 
     Criar um objeto funcionário com:
@@ -147,5 +174,140 @@ let funcionario :{
 }
 console.log(funcionario.supervisores)
 console.log(funcionario.baterPonto(7, 10));
+
+
+// AULA 24 - DEFININDO TIPOS PERSONALIZADOS (ALIAS)
+// quando você precisa usar um tipo objeto por mais de uma vez, não é preciso atribuir o tipo objeto em todos os funcionários
+
+type Funcionario = {
+        supervisores: Array<string>, 
+        baterPonto: (horaEntrada:number, horaSaida:number) => string 
+}
+
+let funcionario2 : Funcionario =
+{
+    supervisores: ['carlos', 'roberto', 'heitor'],
+    baterPonto: function baterPonto(horaEntrada:number, horaSaida:number): string{
+        let horaTrabalho = horaSaida - horaEntrada;
+        if (horaTrabalho <= 8) {
+            return 'Ponto normal';            
+        } else {
+            return 'Ponto fora do horário';
+        }
+    }
+}
+
+let funcionario3 : Funcionario = {
+    supervisores: ['carlos', 'roberto', 'heitor'],
+    baterPonto: function baterPonto(horaEntrada:number, horaSaida:number): string{
+        let horaTrabalho = horaSaida - horaEntrada;
+        if (horaTrabalho <= 8) {
+            return 'Ponto normal';            
+        } else {
+            return 'Ponto fora do horário';
+        }
+    }
+}
+
+// AULA 25 - MÚLTIPLOS COM UNION TYPES
+// esse tipo é para ser usado quando você quer que mais de um tipo de dado seja aceito, mas não todos, como acontece no tipo any. Um exemplo seria poder aceitar number e string mas não boolean. 
+
+let nota: number | string
+
+nota = 10;
+console.log(`Minha nota é ${nota}`);
+
+nota = "Dez"
+console.log(`Minha nota é ${nota}`);
+
+//  nota = true   // <= nesse caso não é aceito
+
+// AULA 26 - CHECANDO TIPOS NO RUNTIME
+// checando variáveis de forma manual
+
+let valor = 10; 
+
+// esse código abaixo não é necessário quando se usa o TypeScript, pois só de colocar o mouse em cima já é possível saber o tipo. Entretanto, é uma maneira de saber o tipo quando é preciso entrar no "mundo JavaScript"
+console.log(`É um ${typeof valor}!`);
+
+// AULA 27 - TIPO NEVER
+// o tipo "never" é quando você quer dizer que ela nunca vai voltar, como quando entra num loop infinito ou entra num tipo de erro. Diferentemente de AUSENCIA de retorno, quando é usado com o tipo "void".
+
+function falha(msg:string) :never{
+    while(true){ //while(true) é um loop infinito
+        throw new Error(msg) //lançar um erro, com a mensagem.
+    }
+}
+
+const produto = {
+    nome: 'Sabão',
+    preco: 4,
+    validarProduto(){
+        if(!this.nome|| this.nome.trim().length == 0) {
+            falha("Precisa ter um nome")
+        }
+        if(this.preco <= 0){
+            falha("Preço inválido")
+        }
+    }
+}
+produto.validarProduto()
+
+// AULA 28 - VALORES OPCIONAIS COM TIPO "NULL"
+// garantia do TypeScript que o usuário irá inserir algum valor
+
+let altura = 12;
+// altura = null; //comando gera erro
+
+let alturaOpcional: null | number = 12
+alturaOpcional = null //Comando aceita null, pois temos um Union Type
+
+type Contato = {
+    nome:string,
+    tell1:string,
+    tell2:string|null
+}
+
+const contato1:Contato = {
+    nome:'Felipe',
+    tell1:'992824882',
+    tell2:null
+}
+
+console.log(contato1.nome);
+console.log(contato1.tell1);
+console.log(contato1.tell2);
+
+// let podeSerNulo:null = null //Não faz sentido ter uma variável que só aceite o tipo null, não tem aplicação em projetos, somente quando é um UNION TYPE, como é o caso do TELL2, que o usuário pode colocar uma string, ou não colocar nada
+
+let podeSerNulo = null
+podeSerNulo = 12
+console.log(podeSerNulo);
+podeSerNulo = "Heyy"
+console.log(podeSerNulo);
+podeSerNulo = null;
+console.log(podeSerNulo);
+
+
+// AULA 29 - DESAFIO TRANFORMAR CÓDIGO JS EM TS
+let contaBancaria:{saldo:number, depositar:(valor:number)=>void} = {
+    saldo: 3456,
+    depositar(valor:number){
+        this.saldo += valor
+    }
+}
+
+let correntista:{nome:string, contaBancaria:{saldo:number, depositar:(valor:number)=>void}, contatos:string[]} = {
+    nome:'Ana Silva',
+    contaBancaria: contaBancaria,
+    contatos:['34567890', '98765432']
+}
+
+correntista.contaBancaria.depositar(3000)
+console.log(correntista);
+
+
+
+
 
 
