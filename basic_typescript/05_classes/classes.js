8da87d9004f8924654aca0e32271b38c7d006547
Felipe Macedo <felipeaamacedo1@gmail.com>
2020-04-20 13:38:49 -0300

    Add control access and Inheritance

--- basic_typescript/05_classes/classes.js
+++ basic_typescript/05_classes/classes.js
@@ -95,5 +95,63 @@ class TProduto3 {
 const prod2 = new TProduto3('Caneca Star Wars', 4.50, 0.10);
 console.log(prod2.resumo());
 //AULA 70 - MODIFICADORES DE ACESSO (PUBLIC, PRIVATE, PROTECTED)
+//são as tags public e private que são colocadas quando se deseja deixar alguns parametos ocultos(private) ou não (public) para os usuários no frontend, no caso dos parametros protected, eles são parametros com o comportamento privado, que entretanto são transmitidos para outras classes que herdam atributos/metodos da classe pai. São colocados antes dos atributos das classes, como no exemplo: private velocidadeAtual:number = 0.
+//CONCLUSÃO:
+//PUBLIC: informações visíveis para todos
+//PRIVADO: informações privadas do mundo não é visível para os outros, estão escondidas dentro da estrutura.
+//PROTECTED: visivel dentro da própria classe, e transmitido por herança
 //
+//PRIVADO < PROTECTED < PUBLIC
+//
+//OBS: Apesar desses tipos de modificadores de acesso existirem no TS, no JS eles não existem, então caso o noEmitOnError estiver = false, apesar de errado no TS, o arquivo JS será gerado, e mesmo tentando acessar o atributo privado no TS e gerando um erro, o erro irá aparecer no browser, pois o JS é gerado de qualquer forma.
+//
+class TCarro {
+    constructor(marca, modelo, velocidadeMaxima = 200) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidadeMaxima = velocidadeMaxima;
+        this.velocidadeAtual = 0;
+    }
+    alterarVelocidade(delta) {
+        const novaVelocidade = this.velocidadeAtual + delta;
+        if (novaVelocidade <= this.velocidadeMaxima && novaVelocidade >= 0) {
+            this.velocidadeAtual = novaVelocidade;
+        }
+        else {
+            this.velocidadeAtual >= this.velocidadeMaxima ? this.velocidadeAtual : this.velocidadeAtual = 0;
+        }
+        return this.velocidadeAtual;
+    }
+    acelerar() {
+        return this.alterarVelocidade(+5);
+    }
+    frear() {
+        return this.alterarVelocidade(-5);
+    }
+}
+let carro1 = new TCarro('Ford', 'Fiesta', 130);
+Array(50).fill(0).forEach(() => carro1.frear()); //O forEach tem como input uma function que é aplicada em todos os campos do Array.
+console.log(`VELOCIDADE ATUAL: ${carro1.frear()}`);
+//AULA 70 - HERANÇA
+//é a capacidade da OOP de reutilizar códigos. Onde uma classe é composta por outra classe, reutilizando o código de outra classe. Ex. Classe TRobo tem dentro Classe TJuntas. É utilizado quando uma classe usa atributos de outra classe no seu código. A herança é criada ao utilizar a palavra 'extends' seguido da classe que ela herda atributos, exemplo
+//
+//class <classe filha> extends <classe pai>
+//
+//class TRobo extends TJunta
+class TFerrari extends TCarro {
+    //como a marca do construtor TCarro vai ser sempre ferrari podemos criar um novo construtor enviando somente 2 atributos, modelo e velocidadeMaxima. Para isso criamos um novo construtor, com os 2 parâmetros de entrada, e usamos dentro dele o parâmetro super() é como você estivesse chamando o construtor PAI e inserindo seus atributos. No caso ferrari, serão 'Ferrari', modelo, e velocidadeMaxima, sendo esses dois últimos inseridos ao criar uma FERRARI.
+    constructor(modelo, velocidadeMaxima) {
+        super('Ferrari', modelo, velocidadeMaxima);
+    }
+    acelerar() {
+        return this.alterarVelocidade(+20);
+    }
+    frear() {
+        return this.alterarVelocidade(-15);
+    }
+}
+const f40 = new TFerrari('Ferrari', 'F40', 324);
+console.log(`${f40.marca} ${f40.modelo}`);
+console.log(f40.acelerar());
+console.log(f40.frear());
 //# sourceMappingURL=classes.js.map
