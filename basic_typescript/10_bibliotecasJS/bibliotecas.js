9d05986bf1be8fab0d71bb335285c4a939a6e57e
Felipe Macedo <felipeaamacedo1@gmail.com>
2020-05-17 03:06:41 -0300

    using JS libraries in Typescript

---
+++ basic_typescript/10_bibliotecasJS/bibliotecas.js
@@ -0,0 +1,10 @@
+"use strict";
+// DUVIDAS
+// desde o primeiro momento em que coloquei o jQuery não gerou esse erro.
+// REFERENCIAS
+// pesquisar os tipos @types para parar com o problema de  tipos do typescript usando bibliotecas de JS.
+// https://microsoft.github.io/TypeSearch/
+//https://www.typescriptlang.org/docs/handbook/declaration-files/by-example.html
+// declare const $:any
+$('body').append('Usando jQuery');
+//# sourceMappingURL=bibliotecas.js.map
---
+++ basic_typescript/10_bibliotecasJS/bibliotecas.ts
@@ -0,0 +1,14 @@
+// DUVIDAS
+// desde o primeiro momento em que coloquei o jQuery não gerou esse erro.
+
+// REFERENCIAS
+// pesquisar os tipos @types para parar com o problema de  tipos do typescript usando bibliotecas de JS.
+// https://microsoft.github.io/TypeSearch/
+
+//https://www.typescriptlang.org/docs/handbook/declaration-files/by-example.html
+
+
+// declare const $:any
+
+$('body').append('Usando jQuery')
+
